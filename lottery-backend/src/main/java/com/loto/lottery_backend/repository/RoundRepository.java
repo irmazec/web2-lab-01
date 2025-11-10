23342d736865bf2acd149e6f8f8627501ba0f169
irmazecevic <irmazecevic0@gmail.com>
2025-11-10 18:07:07 +0100

    fix

--- lottery-backend/src/main/java/com/loto/lottery_backend/repository/RoundRepository.java
+++ lottery-backend/src/main/java/com/loto/lottery_backend/repository/RoundRepository.java
@@ -4,9 +4,11 @@ import com.loto.lottery_backend.entity.Round;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public interface RoundRepository extends JpaRepository<Round, UUID>, JpaSpecificationExecutor<Round> {
     Optional<Round> findFirstByOrderByStartedAtDesc();
 }
